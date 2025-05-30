b719dce92c7d95e2d9fb66d26da4321c0dec1dda
Robale <henokrobale@gmail.com>
2025-05-30 03:27:01 -0500

    Initial Commit

---
+++ WordEase/WordEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordEaseApp.swift
+//  WordEase
+//
+//  Created by Henok Robale on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WordEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
